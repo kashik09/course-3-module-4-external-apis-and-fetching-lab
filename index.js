0cc5679024cc1a2dcf2fdc62b99385a509bf22da
kashik09 <kashiku789@gmail.com>
2025-12-19 10:58:26 +0300

    Add DOM element references

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // index.js
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
-// Your code here!
+// Get DOM elements
+const stateInput = document.getElementById('state-input');
+const getAlertsBtn = document.getElementById('get-alerts-btn');
+const alertsContainer = document.getElementById('alerts-container');
+const errorMessage = document.getElementById('error-message');
